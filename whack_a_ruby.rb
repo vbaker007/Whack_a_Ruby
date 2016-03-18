ab5dc720e09a76a8eaf8ebe11786f92bd1a42a1f
vbaker007 <vbaker007@hotmail.com>
2016-03-17 22:16:58 -0400

    Add Ruby & Hammer Images

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -4,6 +4,31 @@ class WhackaRuby < Gosu::Window
   def initialize 
     super(800, 600)
     self.caption = 'Whack the Ruby!'
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
+    @visible = 0
+    @hammer_image = Gosu::Image.new('hammer.png')
+  end
+#window = WhackaRuby.new
+#window.show
+  def draw
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    end
+  end
+
+  def update
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
 end 
 
