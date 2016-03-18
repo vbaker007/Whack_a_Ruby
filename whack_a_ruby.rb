5b6d229dbcc0a738ef40f236ec38f18ec6bc3df8
vbaker007 <vbaker007@hotmail.com>
2016-03-17 22:54:46 -0400

    Expanded upon the Update Method

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -14,6 +14,9 @@ class WhackaRuby < Gosu::Window
     @visible = 0
     @hammer_image = Gosu::Image.new('hammer.png')
     @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
+    @playing = true
   end
 #window = WhackaRuby.new
 #window.show
@@ -32,22 +35,39 @@ class WhackaRuby < Gosu::Window
     end
     draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
     @hit = 0
+    @font.draw(@time_left.to_s, 20, 20, 2)
+    @font.draw(@score.to_s, 700, 20, 2)
+    unless @playing
+      @font.draw('Game Over', 300, 300, 3)
+      @font.draw('Press the Space Bar to Play Again', 175, 350, 3)
+      @visible = 20
+    end 
   end
 
   def update
-    @x += @velocity_x
-    @y += @velocity_y
-    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
-    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
-    @visible -= 1
-    @visible = 30 if @visible < -10 && rand < 0.01
+    if @playing
+      @x += @velocity_x
+      @y += @velocity_y
+      @visible -= 1
+      @time_left = (100 - (Gosu.milliseconds - @start_time / 1000))
+      @playing = false if @time_left < 0 
+      @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+      @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+      @visible -= 1
+      @visible = 30 if @visible < -10 and rand < 0.01
+    end
   end
+
   def button_down(id)
-    if (id == Gosu::MsLeft)
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && visible >= 0
-        @hit = 1
-      else
-        @hit = -1
+    if @playing
+      if (id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && visible >= 0
+          @hit = 1
+          @score += 5
+        else
+          @hit = -1
+          @score -= 1
+        end
       end
     end
  end
