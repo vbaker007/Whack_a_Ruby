65dba770cd1239e6067be465fe4dc630db68c5c5
vbaker007 <vbaker007@hotmail.com>
2016-03-17 21:33:14 -0400

    Add initialize method to Gosu Window

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -1,5 +1,11 @@
 require 'gosu'
 
 class WhackaRuby < Gosu::Window
-
+  def initialize 
+    super(800, 600)
+    self.caption = 'Whack the Ruby!'
+  end
 end 
+
+window = WhackaRuby.new
+window.show
