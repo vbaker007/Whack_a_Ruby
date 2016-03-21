501f4e08b47f030e44fd41878cd5e39314a3acba
vbaker007 <vbaker007@hotmail.com>
2016-03-21 03:28:13 -0400

    Add Play Again Option

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -17,18 +17,19 @@ class WhackaRuby < Gosu::Window
     @font = Gosu::Font.new(30)
     @score = 0
     @playing = true
+    @start_time = 0
   end
 
   def update
     if @playing
       @x += @velocity_x
       @y += @velocity_y
-      @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
-      @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
       @visible -= 1
-      @visible = 30 if @visible < -10 and rand < 0.01
       @time_left = (100 - (Gosu.milliseconds / 1000 ))
       @playing = false if @time_left < 0
+      @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+      @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+      @visible = 30 if @visible < -10 and rand < 0.01
     end
   end
 
@@ -50,6 +51,7 @@ class WhackaRuby < Gosu::Window
     @font.draw(@score.to_s, 700, 20, 2)
     unless @playing
       @font.draw('Game Over', 300, 300, 3)
+      @font.draw('Press the Space Bar to Play Again', 175, 350, 3)
       @visible = 20
     end
   end
@@ -64,6 +66,13 @@ class WhackaRuby < Gosu::Window
           @hit = -1
           @score -= 1
         end
+      end
+    else 
+      if (id == Gosu::KbSpace)
+        @playing = true
+        @visible = -10
+        @start_time = Gosu.milliseconds
+        @score = 0
       end
     end
   end
